6d0dfa9fadb9fb1be61fc960ec658eeb441e6823
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-25 13:56:44 -0300

    wip: update svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess({})],
+  extensions: ['.svelte', '.md'],
+  preprocess: [vitePreprocess({})],
 
-	kit: {
-		alias: {
-			$data: './src/data'
-		},
-		adapter: adapter()
-	}
+  kit: {
+    alias: {
+      $data: './src/data'
+    },
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    })
+  }
 };
 
 export default config;
