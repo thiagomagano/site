5dfdf704f78caa4417c0e5f7e1a0273c28f292b2
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-25 14:19:00 -0300

    prepare: remove skeleton config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,6 @@
-import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
-import { skeleton } from '@skeletonlabs/tw-plugin';
-
 /** @type {import('tailwindcss').Config}*/
-const config: Config = {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
-
-	theme: {
-		extend: {}
-	},
-
-	plugins: [
-		skeleton({
-			themes: {
-				preset: [{ name: 'wintry', enhancements: true }]
-			}
-		})
-	]
-};
+const config: Config = {};
 
 module.exports = config;
