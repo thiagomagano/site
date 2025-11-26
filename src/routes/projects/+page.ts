3bac86ff2f5029676bcfd70201cdb2fb228394dd
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-26 17:50:51 -0300

    config vite to proxy localhost api

--- src/routes/projects/+page.ts
+++ src/routes/projects/+page.ts
@@ -1,9 +1,8 @@
 import type { PageLoad } from './$types';
 
-const API_URL = "http://localhost:3000/api"
 
 export const load: PageLoad = async ({ fetch }) => {
-  const res = await fetch(API_URL + "/projects");
+  const res = await fetch("api/projects");
   const projects = await res.json();
 
   return { projects };
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
-});
+export default defineConfig(({ mode }) => ({
+
+  plugins: [tailwindcss(), sveltekit()],
+  server: mode === 'development' ? {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  } : {}
+
+}));
