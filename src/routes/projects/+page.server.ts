261e23e95c901e7655eba4222dfcdce8f0aded42
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-28 13:33:03 -0300

    fix: prerender on /projects

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,5 +1,7 @@
 import type { PageServerLoad } from './$types';
 
+export const prerender = false;
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const load: PageServerLoad = async ({ fetch }) => {
