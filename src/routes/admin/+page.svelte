e1df26b9feab1aa83c3824a4046f8a6d30c45419
Thiago Magano <thiagomagano1993@outlook.com>
2025-12-01 17:08:46 -0300

    wip:admin login page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Headline from '$lib/components/Headline.svelte';
+</script>
+
+<section>
+	<div class="container py-16 mx-auto max-w-(--breakpoint-lg)">
+		<form class="w-full space-y-4 p-4 place-self-center max-w-md">
+			<fieldset>
+				<h2 class="h2 text-center">Login</h2>
+			</fieldset>
+
+			<fieldset class="space-y-4">
+				<label class="label">
+					<span class="label-text">Email</span>
+					<input class="input" type="email" placeholder="Email" />
+				</label>
+			</fieldset>
+			<fieldset class="space-y-4">
+				<label class="label">
+					<span class="label-text">Senha</span>
+					<input class="input" type="password" placeholder="Senha" />
+				</label>
+			</fieldset>
+			<fieldset class="flex justify-end">
+				<button type="button" class="btn preset-filled-primary-500">Submit</button>
+			</fieldset>
+		</form>
+	</div>
+</section>
