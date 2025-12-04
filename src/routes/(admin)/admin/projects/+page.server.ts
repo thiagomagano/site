5702bf1e4329d189911580e44fff04e55613c3ed
Thiago Magano <thiagomagano1993@outlook.com>
2025-12-04 14:43:22 -0300

    add: some styles in admin/projects

--- src/routes/(admin)/admin/projects/+page.server.ts
+++ src/routes/(admin)/admin/projects/+page.server.ts
@@ -1,5 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import { apiClient } from '$lib/api/client.js';
+import type { Project } from '$data/projects.js';
 
 export async function load({ locals, cookies }) {
 	// O hook já verifica autenticação, mas podemos verificar novamente
@@ -10,7 +11,7 @@ export async function load({ locals, cookies }) {
 	try {
 		// Requisição autenticada - cookies são passados automaticamente
 		const projects = await apiClient.get('/projects', {}, cookies);
-
+		console.log(projects);
 		return {
 			projects,
 			user: locals.user
--- src/routes/(admin)/admin/projects/+page.svelte
+++ src/routes/(admin)/admin/projects/+page.svelte
@@ -1,11 +1,55 @@
-<script>
+<script lang="ts">
+	import type { Project } from '$data/projects';
+	import { FilePenLine, PackagePlus, Trash } from '@lucide/svelte';
+
 	let { data } = $props();
-	const { projects } = data;
+	const projects = data.projects as Project[];
 </script>
 
-<h1>PROJETOS</h1>
-<ol class="ol">
-	{#each projects as project}
-		<li id={project.id}>{project.title}</li>
-	{/each}
-</ol>
+<main>
+	<h1>Projetos</h1>
+	<div class="flex w-full items-end justify-end">
+		<button type="button" class="btn preset-filled-primary-500">
+			<span>Novo Projeto</span>
+			<PackagePlus size={18} />
+		</button>
+	</div>
+
+	<div class="table-wrap">
+		<table class="table caption-top">
+			<caption class="pt-4">Projetos</caption>
+			<thead>
+				<tr>
+					<th>Titulo</th>
+					<th>Links</th>
+					<th>Ações</th>
+				</tr>
+			</thead>
+			<tbody class="[&>tr]:hover:preset-tonal-primary">
+				{#each projects as project}
+					<tr>
+						<td><a href={'/admin/projects/' + project._id}>{project.title}</a></td>
+						<td
+							><a class="text-indigo-500 hover:underline" href={project.link}>Demo</a> |
+							<a class="text-indigo-500 hover:underline" href={project.github}>Github</a></td
+						>
+						<td class="flex gap-2">
+							<button type="button" class="btn-icon" title="Edit" aria-label="Edit">
+								<FilePenLine />
+							</button>
+							<button type="button" class="btn-icon" title="Delete" aria-label="Delete">
+								<Trash />
+							</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+			<tfoot>
+				<tr>
+					<td colspan="2">Total</td>
+					<td>{projects.length} Projetos</td>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+</main>
